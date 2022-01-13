215e1b304ee6bb2bf4b2f061e749616f3c7ca327
MarcosSesa <marcoxxtee@gmail.com>
2022-01-13 17:28:07 +0100

    Creadas variables de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  SupabaseApiUrl: 'https://stfgpujtnwmzjltghsis.supabase.co',
+  SupabaseApiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTgwMzQyMywiZXhwIjoxOTU3Mzc5NDIzfQ.Be2xHvLsCGW3W9x_CTRvUoLf6qjvb6J0eAs0ELndNJ8'
 };
 
 /*
